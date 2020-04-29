420ce5d48d55974607c7518e90beeac65d4fe4dc
angryboo <buyongsong@gmail.com>
2020-04-29 09:48:59 +0900

    fix: game set logic

--- public/main.js
+++ public/main.js
@@ -1,10 +1,13 @@
 const $goData = document.querySelector('.go-data');
 const $goItem = document.querySelectorAll('.go-item');
+const $resetMain = document.querySelector('.reset-main');
+const $setMsg = document.querySelector('.set-msg');
 
 // status
 
 let turn = '';
 let omok = [];
+let game = true;
 const checkArray = Array.from({ length: 11 }, () => Array.from({ length: 11 }, () => ''));
 
 
@@ -24,6 +27,27 @@ const setStone = () => {
   });
 };
 
+
+// 이니셜
+const init = async () => {
+  const { data } = await axios.patch('/omok/init');
+  omok = data;
+  omok.forEach(({ player }, i) => {
+    $goItem[i].firstElementChild.classList.add(`${player}`);
+    if (player === 'none') {
+      $goItem[i].firstElementChild.classList.remove('black');
+      $goItem[i].firstElementChild.classList.remove('white');
+    }
+  });
+  getTurn();
+  game = true;
+  $setMsg.textContent = '';
+};
+
+const setGame = arrData => {
+  game = false;
+  $setMsg.textContent = `승리는 ${arrData[0].player} 입니다!`;
+};
 
 // 횡방향 check 함수
 const checkOmokHzt = () => {
@@ -46,6 +70,7 @@ const checkOmokHzt = () => {
           xaxis: j
         }];
         if (hzt.length === 5) {
+          setGame(hzt);
           console.log('[victory!]', hzt);
         }
       } else {
@@ -77,6 +102,7 @@ const checkOmokVtl = () => {
           xaxis: j
         }];
         if (vtl.length === 5) {
+          setGame(vtl);
           console.log('[victory!]', vtl);
         }
       } else {
@@ -87,18 +113,144 @@ const checkOmokVtl = () => {
 };
 
 
-// 대각선 정방향 check 함수
-const checkOmokDiagonalFw = () => {
-  let diagonalFw = [];
+// 대각선 West check 함수
+const checkOmokDiagonalWS = () => {
+  let diagonal = [];
 
   let diagonalCount = checkArray[0].length - 1;
   for (let i = 1; i < checkArray.length - 1; i++) {
     for (let j = 1; j < diagonalCount; j++) {
       if (checkArray[i + j - 1][j] !== 'none' && checkArray[i + j - 1][j] === checkArray[i + j - 2][j - 1]) {
         console.log(111);
+        diagonal = diagonal.length === 0 ? [...diagonal, {
+          player: checkArray[i + j - 2][j - 1],
+          yaxis: i + j - 2,
+          xaxis: j - 1
+        }, {
+          player: checkArray[i + j - 1][j],
+          yaxis: i + j - 1,
+          xaxis: j
+        }] : [...diagonal, {
+          player: checkArray[i + j - 1][j],
+          yaxis: i + j - 1,
+          xaxis: j
+        }];
+        if (diagonal.length === 5) {
+          setGame(diagonal);
+          console.log('[victory! WS]', diagonal);
+        }
+      } else {
+        diagonal = [];
       }
     }
     diagonalCount -= 1;
+  }
+};
+
+
+// 대각선 WN check 함수
+const checkOmokDiagonalWN = () => {
+  let diagonal = [];
+
+  let diagonalCount = checkArray[0].length - 1;
+  for (let i = 2; i < checkArray.length - 1; i++) {
+    for (let j = 1; j < diagonalCount; j++) {
+      // console.log(j, i + j - 1);
+      if (checkArray[j][i + j - 1] !== 'none' && checkArray[j][i + j - 1] === checkArray[j - 1][i + j - 2]) {
+        diagonal = diagonal.length === 0 ? [...diagonal, {
+          player: checkArray[j - 1][i + j - 2],
+          yaxis: j - 1,
+          xaxis: i + j - 2
+        }, {
+          player: checkArray[j][i + j - 1],
+          yaxis: j,
+          xaxis: i + j - 1
+        }] : [...diagonal, {
+          player: checkArray[j][i + j - 1],
+          yaxis: j,
+          xaxis: i + j - 1
+        }];
+        if (diagonal.length === 5) {
+          setGame(diagonal);
+          console.log('[victory! WN]', diagonal);
+        }
+      } else {
+        diagonal = [];
+      }
+    }
+    diagonalCount -= 1;
+  }
+};
+
+
+// 대각선 ES check 함수
+const checkOmokDiagonalES = () => {
+  let diagonal = [];
+
+  let diagonalCount = 0;
+  for (let i = 1; i < checkArray.length - 1; i++) {
+    let loofCount = 0;
+    for (let j = checkArray[0].length - 2; j >= diagonalCount; j--) {
+      // console.log(i + loofCount, j);
+      if (checkArray[i + loofCount][j] !== 'none' && checkArray[i + loofCount][j] === checkArray[i + loofCount - 1][j + 1]) {
+        diagonal = diagonal.length === 0 ? [...diagonal, {
+          player: checkArray[i + loofCount - 1][j + 1],
+          yaxis: i + loofCount - 1,
+          xaxis: j + 1
+        }, {
+          player: checkArray[i + loofCount][j],
+          yaxis: i + loofCount,
+          xaxis: j
+        }] : [...diagonal, {
+          player: checkArray[i + loofCount][j],
+          yaxis: i + loofCount,
+          xaxis: j
+        }];
+        if (diagonal.length === 5) {
+          setGame(diagonal);
+          console.log('[victory! ES]', diagonal);
+        }
+      } else {
+        diagonal = [];
+      }
+      loofCount += 1;
+    }
+    diagonalCount += 1;
+  }
+};
+
+
+// 대각선 EN check 함수
+const checkOmokDiagonalEN = () => {
+  let diagonal = [];
+
+  let loofCount = 0;
+  for (let i = 0; i < checkArray.length - 2; i++) {
+    for (let j = 1; j < loofCount + 2; j++) {
+      // console.log(j, i - j + 1);
+      if (checkArray[j][i - j + 1] !== 'none' && checkArray[j][i - j + 1] === checkArray[j - 1][i - j + 2]) {
+        diagonal = diagonal.length === 0 ? [...diagonal, {
+          player: checkArray[j - 1][i - j + 2],
+          yaxis: j - 1,
+          xaxis: i - j + 2
+        }, {
+          player: checkArray[j][i - j + 1],
+          yaxis: j,
+          xaxis: i - j + 1
+        }] : [...diagonal, {
+          player: checkArray[j][i - j + 1],
+          yaxis: j,
+          xaxis: i - j + 1
+        }];
+        if (diagonal.length === 5) {
+          setGame(diagonal);
+          console.log('[victory! EN]', diagonal);
+        }
+      } else {
+        diagonal = [];
+      }
+    }
+    loofCount += 1;
   }
 };
 
@@ -114,7 +266,10 @@ const setCheckArray = data => {
   });
   checkOmokHzt();
   checkOmokVtl();
-  checkOmokDiagonalFw();
+  checkOmokDiagonalWS();
+  checkOmokDiagonalWN();
+  checkOmokDiagonalES();
+  checkOmokDiagonalEN();
 };
 
 
@@ -164,7 +319,7 @@ const getAxis = target => {
 
 // click event
 $goData.addEventListener('click', ({ target }) => {
-  if (!target.matches('.go-data > div')) return;
+  if (!target.matches('.go-data > div') || !game) return;
   const select = getAxis(target);
   console.log('[select-axis]', select);
   let isPossible = false;
@@ -176,4 +331,11 @@ $goData.addEventListener('click', ({ target }) => {
   if (isPossible) return;
   setData(select);
   changeTurn();
+});
+
+
+// reset
+$resetMain.addEventListener('click', () => {
+  console.log(111);
+  init();
 });
